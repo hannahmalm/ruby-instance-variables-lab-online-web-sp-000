84ba1058895e7627482bc9ea3579aeba74f5cf7a
Hannah Malm <hannahemalm616@gmail.com>
2020-12-25 18:31:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,19 @@
-
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end
+ 
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+puts lassie.name
+ 
 
 #defined the name= method and the name method 
 #The name method is responsible for reporting, or reading the name 
